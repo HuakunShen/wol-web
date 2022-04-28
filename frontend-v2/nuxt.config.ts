a5e7204ce4f524f71024481bc96d1d6f64a924dc
Huakun Shen <huakun.shen@huakunshen.com>
2022-04-28 02:57:56 -0400

    Add tailwind plugin

--- frontend-v2/nuxt.config.ts
+++ frontend-v2/nuxt.config.ts
@@ -1,6 +1,16 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  modules: ['@nuxtjs/tailwindcss'],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
---
+++ frontend-v2/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<h1 class="underline">hello</h1>
+		<button class="border-4 rounded text-blue-500 px-5 py-2">btn</button>
+	</div>
+</template>
